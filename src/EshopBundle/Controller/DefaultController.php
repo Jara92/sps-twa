98379187b60950fbadbf1c4f26e47d5e51b2b3e1
Jara92 <jara.fikar@seznam.cz>
2018-02-19 14:35:26 +0100

    Tvorba stranky

--- src/EshopBundle/Controller/DefaultController.php
+++ src/EshopBundle/Controller/DefaultController.php
@@ -10,8 +10,19 @@ class DefaultController extends Controller
     /**
      * @Route("/")
      */
+    // rout definuje url adresu
     public function indexAction()
     {
-        return $this->render('EshopBundle:Default:index.html.twig');
+        // Instance databázového připojení; Nese informace o databázi...
+        $entity_manager = $this->getDoctrine()->getManager();
+        // repozitář pro práci
+        $product_repository = $entity_manager->getRepository("EshopBundle:Product");
+        // vyhledá všechny záznamy v dané tabulce
+        $products = $product_repository->findAll();
+        
+        // array - Parametr šablony
+        return $this->render('@Eshop\Default\index.html.twig', array(
+            "products" => $products
+        ));
     }
 }
